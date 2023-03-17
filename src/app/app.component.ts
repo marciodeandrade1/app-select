a64b1516ecf7a639a647359a61d369aea8419df7
Márcio de Andrade <gmandrade1@hotmail.com>
2023-03-17 06:50:18 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-select';
+
+  checkboxValues: boolean[] = [false, false,false,false,false ];
+
+  selectAll: boolean = false;
+
+  selectAllCheckbox() {
+  for (let i = 0; i < this.checkboxValues.length; i++) {
+    this.checkboxValues[i] = this.selectAll;
+  }
+  console.log(this.selectAllCheckbox);
+}
+
+  updateSelectAll() {
+  this.selectAll = this.checkboxValues.every(val => val);
+  console.log(this.updateSelectAll)
+}
+
+
 }
